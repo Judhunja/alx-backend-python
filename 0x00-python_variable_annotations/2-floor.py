cecc1c0de310cbe3caad766912d0279782f03d9b
Judhunja <judxhunja@gmail.com>
2024-01-11 16:06:13 +0300

    float to int

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function floor."""
+import math
+
+
+def floor(n: float) -> int:
+    """Remove decimal part of a float."""
+    return math.floor(n)
