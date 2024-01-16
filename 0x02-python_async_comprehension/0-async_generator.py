311a96cd1a8b2ccf4644abd41e095663c0b96c7e
Judhunja <judxhunja@gmail.com>
2024-01-16 18:02:36 +0300

    wait for 1 second then yield

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,5 +8,5 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """Yield random numbers between 0 and 10 in a loop."""
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
