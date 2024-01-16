db6328cb1959b3d64fca3b85832e59b2c892c707
Judhunja <judxhunja@gmail.com>
2024-01-16 17:21:25 +0300

    changing return type to AsyncGenerator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This module contains a coroutine async_comprehension."""
-from typing import Generator, List
+from typing import AsyncGenerator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[List[float], None, None]:
+async def async_comprehension() -> AsyncGenerator[List[float], None]:
     """Collect 10 random numbers using an async comprehension."""
     return [i async for i in (async_generator())]
