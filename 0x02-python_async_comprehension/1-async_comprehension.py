bef9d9c63538ccf16a698ba78bd3c1bba03a5272
Judhunja <judxhunja@gmail.com>
2024-01-16 17:25:43 +0300

    removing brackets around function in async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This module contains a coroutine async_comprehension."""
-from typing import AsyncGenerator, List
+from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> AsyncGenerator[List[float], None]:
+async def async_comprehension() -> Generator[List[float], None, None]:
     """Collect 10 random numbers using an async comprehension."""
-    return [i async for i in (async_generator())]
+    return [i async for i in async_generator()]
