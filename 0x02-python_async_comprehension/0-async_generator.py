cf42a28186161800ef1f8303821ecf700feda117
Judhunja <judxhunja@gmail.com>
2024-01-16 16:55:17 +0300

    asynchronously wait then yield a random number

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This module contains a coroutine async_generator."""
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Yield random numbers between 0 and 10 in a loop."""
+    for _ in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,4 @@
+### 0x02. Python - Async Comprehension
+- asynchronous generator
+- async comprehensions
+- type-annotated generators
