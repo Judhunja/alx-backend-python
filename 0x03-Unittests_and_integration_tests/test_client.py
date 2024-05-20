3eb9bfe6c8b3ee4f6225502ed83e7991e39c4315
Judhunja <judxhunja@gmail.com>
2024-05-20 14:23:46 +0300

    getting the value of a mocked property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 import client
 
@@ -29,6 +29,23 @@ class TestGithubOrgClient(unittest.TestCase):
             GithubOrgClient.ORG_URL.format(org=value)
         )
         self.assertEqual(result, {})
+
+    def test_public_repos(self):
+        """Test that GitHubOrgClient._public_repos_url works properly"""
+        with patch(
+            "client.GithubOrgClient.org", new_callable=PropertyMock
+        ) as mock_client_url:
+            mock_client_url.return_value = {
+                "repos_url":
+                    "https://api.github.com/orgs/exampleorg/examplerepo"
+            }
+            result = client.GithubOrgClient._public_repos_url
+            # using fget to retrieve the value of org property
+            # ["repos_url"]. fget searches the org property
+            # of client.GithubOrgClient
+            result_val = result.fget(client.GithubOrgClient)
+            expected_res = "https://api.github.com/orgs/exampleorg/examplerepo"
+            self.assertEqual(result_val, expected_res)
 
 
 if __name__ == "__main__":
